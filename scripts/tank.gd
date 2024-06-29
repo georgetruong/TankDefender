110a01b137c7b1783a9b2a17948b975db5a90d86
George Truong <truong.george@gmail.com>
2024-06-29 09:58:14 -0700

    Draw dashed line

--- scripts/tank.gd
+++ scripts/tank.gd
@@ -7,9 +7,7 @@ var target_position = null
 @onready var target_line = $TargetLine
 
 func _ready():
-	target_line.default_color = Color.GREEN
-	target_line.width = 1
-	target_line.points = [Vector2.ZERO, Vector2.ZERO]
+	setup_target_line()
 
 func _process(delta):
 	if !target_position:
@@ -33,11 +31,34 @@ func _physics_process(delta):
 
 	update_line()
 
+############################################################################################################################
+# Target Line
+#
+############################################################################################################################
+func setup_target_line():
+
+	#target_line.default_color = Color.GREEN
+	target_line.width = 2
+	target_line.points = [Vector2.ZERO, Vector2.ZERO]
+
+	var dash_texture = create_dash_texture(3, Color.GREEN)
+	target_line.texture = dash_texture
+	target_line.texture_mode = Line2D.LINE_TEXTURE_TILE
+	target_line.texture_repeat = CanvasItem.TEXTURE_REPEAT_ENABLED
+
+func create_dash_texture(dash_size: int, dash_color: Color):
+	var image = Image.create(dash_size * 2, 1, false, Image.FORMAT_RGBA8)
+	image.fill(Color.TRANSPARENT)
+	for i in range(dash_size): 
+		image.set_pixel(i, 0, dash_color)
+	var texture = ImageTexture.create_from_image(image)
+	return texture
+
 func update_line():
 	# Coordinates local relative to character
 	if target_position:
-		target_line.points[0] = Vector2.ZERO 
+		target_line.points[0] = Vector2.ZERO
 		target_line.points[1] = to_local(target_position)
 	else:
-		target_line.points[0] = Vector2.ZERO 
+		target_line.points[0] = Vector2.ZERO
 		target_line.points[1] = Vector2.ZERO
