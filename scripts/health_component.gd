6dc6a0c2db5471a3c000bba6b2003fa21331b99d
George Truong <truong.george@gmail.com>
2024-07-09 14:25:12 -0700

    Fix rotation and display of health bars

--- scripts/health_component.gd
+++ scripts/health_component.gd
@@ -6,14 +6,23 @@ var health : float
 
 @onready var health_bar = $HealthBar
 
+@export var offset = Vector2(0, -10)
+
 func _ready():
+	top_level = true
+
 	health = MAX_HEALTH
 	hide_health_bar()
 
 func _process(delta):
+	global_position = get_parent().global_position + offset
+	global_rotation = 0
+
 	health_bar.update_health(health, MAX_HEALTH)
-	if health <  MAX_HEALTH:
+	if health > 0 and health <  MAX_HEALTH:
 		show_health_bar()
+	else:
+		hide_health_bar()
 
 func show_health_bar():
 	health_bar.show()
@@ -26,6 +35,7 @@ func damage(amount: float):
 	health -= amount
 	health = max(health, 0)
 	if health <= 0:
+		hide_health_bar()
 		die()
 	else:
 		health_bar.update_health(health, MAX_HEALTH)
@@ -36,4 +46,5 @@ func heal(amount: float):
 	health_bar.update_health(health, MAX_HEALTH)
 
 func die():
+	hide_health_bar()
 	get_parent().die()
